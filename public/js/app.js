a4e1b47f5e7fa981d694e609642568378e761b69
Mai Trung Duc <maitrungduc1410@gmail.com>
2019-11-27 21:24:23 +0800

    revert .env.example

--- public/js/app.js
+++ public/js/app.js
@@ -9136,8 +9136,8 @@ var router = new vue_router__WEBPACK_IMPORTED_MODULE_1__["default"]({
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /workspace/laravel/private-channel-realtime-chat/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /workspace/laravel/private-channel-realtime-chat/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/maitrungduc/Laravel_Projects/private-realtime-chat/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/maitrungduc/Laravel_Projects/private-realtime-chat/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
